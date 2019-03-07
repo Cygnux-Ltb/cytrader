d22274964c08d9a374205c2d14a4ce7514d50691
yellow013 <wk9988@gmail.com>
2019-03-07 10:30:14 +0800

    update adaptor api

--- core/src/main/java/io/ffreedom/redstone/core/adaptor/OutboundAdaptor.java
+++ core/src/main/java/io/ffreedom/redstone/core/adaptor/OutboundAdaptor.java
@@ -1,33 +1,30 @@
 package io.ffreedom.redstone.core.adaptor;
 
+import org.slf4j.Logger;
+
 import io.ffreedom.common.fsm.Enable;
+import io.ffreedom.common.log.CommonLoggerFactory;
 import io.ffreedom.redstone.core.account.Account;
-import io.ffreedom.redstone.core.adaptor.dto.ReplyBalance;
-import io.ffreedom.redstone.core.adaptor.dto.ReplyPositions;
 import io.ffreedom.redstone.core.adaptor.dto.SubscribeMarketData;
 import io.ffreedom.redstone.core.order.Order;
 
-public abstract class OutboundAdaptor<SMD extends SubscribeMarketData, RP extends ReplyPositions, RB extends ReplyBalance>
-		extends AbstractAdaptor implements Enable {
+public abstract class OutboundAdaptor extends AbstractAdaptor implements Enable {
+
+	protected Logger logger = CommonLoggerFactory.getLogger(getClass());
 
 	public OutboundAdaptor(int adaptorId, String adaptorName) {
 		super(adaptorId, adaptorName);
 	}
 
-	public abstract boolean subscribeMarketData(SMD subscribeMarketData);
+	public abstract boolean subscribeMarketData(SubscribeMarketData subscribeMarketData);
 
 	public abstract boolean newOredr(Order order);
 
 	public abstract boolean cancelOrder(Order order);
 
-	public boolean modifyOrder(Order order) {
+	public abstract boolean queryPositions(Account account);
 
-		return false;
-	}
-
-	public abstract RP queryPositions(Account account);
-
-	public abstract RB queryBalance(Account account);
+	public abstract boolean queryBalance(Account account);
 
 	private boolean isEnabled;
 
