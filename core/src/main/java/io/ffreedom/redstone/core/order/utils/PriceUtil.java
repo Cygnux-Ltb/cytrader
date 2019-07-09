10985643c49000b343027ef4e061c8226ecaa3e1
yellow013 <wk9988@gmail.com>
2019-07-09 16:55:15 +0800

    update order api

---
+++ core/src/main/java/io/ffreedom/redstone/core/order/utils/PriceUtil.java
@@ -0,0 +1,27 @@
+package io.ffreedom.redstone.core.order.utils;
+
+public final class PriceUtil {
+
+	private static final long longMultiplier = 100000000L;
+
+	private static final double doubleMultiplier = 100000000.0D;
+
+	public final static long doublePriceToLong(double price) {
+		return (long) (price * longMultiplier);
+	}
+
+	public final static double longPriceToDouble(long price) {
+		return price / doubleMultiplier;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(PriceUtil.doublePriceToLong(4.981312));
+		
+		System.out.println(PriceUtil.longPriceToDouble(4981312));
+		
+	}
+
+}
