306ce6ce84fe325cf49c9b3ecfe09a0ab1628a3d
yellow013 <wk9988@gmail.com>
2019-09-11 21:30:26 +0800

    update actor

--- engine/src/main/java/io/ffreedom/redstone/actor/OrderDumpActor.java
+++ engine/src/main/java/io/ffreedom/redstone/actor/OrderDumpActor.java
@@ -1,5 +1,15 @@
 package io.ffreedom.redstone.actor;
 
-public final class OrderDumpActor {
+import com.lmax.disruptor.EventHandler;
+
+import io.ffreedom.redstone.core.order.api.Order;
+
+public final class OrderDumpActor implements EventHandler<Order> {
+
+	@Override
+	public void onEvent(Order event, long sequence, boolean endOfBatch) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
 
 }
