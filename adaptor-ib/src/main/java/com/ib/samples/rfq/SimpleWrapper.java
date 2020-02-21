f6ce7a52fe7a38750debad6899ecc4fcede418e5
yellow013 <wk9988@gmail.com>
2020-02-21 22:46:13 +0800

    add ib rfq

--- adaptor-ib/src/main/java/com/ib/samples/rfq/SimpleWrapper.java
+++ adaptor-ib/src/main/java/com/ib/samples/rfq/SimpleWrapper.java
@@ -1,7 +1,7 @@
 /* Copyright (C) 2013 Interactive Brokers LLC. All rights reserved.  This code is subject to the terms
  * and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable. */
 
-package samples.rfq;
+package com.ib.samples.rfq;
 
 import java.io.File;
 import java.io.IOException;
