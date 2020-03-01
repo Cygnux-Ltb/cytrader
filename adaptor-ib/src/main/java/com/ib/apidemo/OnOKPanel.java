ef40e46573bdb6fbed20179c8a55239e278e838e
yellow013 <wk9988@gmail.com>
2020-03-01 22:09:39 +0800

    update renamed

--- adaptor-ib/src/main/java/com/ib/apidemo/OnOKPanel.java
+++
@@ -1,8 +0,0 @@
-package com.ib.apidemo;
-
-import com.ib.apidemo.util.VerticalPanel;
-import com.ib.client.OrderCondition;
-
-public abstract class OnOKPanel extends VerticalPanel {
-	public abstract OrderCondition onOK();
-}
