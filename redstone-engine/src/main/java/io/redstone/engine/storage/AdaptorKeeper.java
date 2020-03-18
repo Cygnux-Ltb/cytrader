b9948d3d598e443ab47b767117bd4a59640cd64d
yellow013 <wk9988@gmail.com>
2020-03-18 22:39:19 +0800

    update adaptor keeper

--- redstone-engine/src/main/java/io/redstone/engine/storage/AdaptorKeeper.java
+++ redstone-engine/src/main/java/io/redstone/engine/storage/AdaptorKeeper.java
@@ -3,23 +3,23 @@ package io.redstone.engine.storage;
 import org.eclipse.collections.api.map.primitive.MutableIntObjectMap;
 
 import io.mercury.common.collections.MutableMaps;
-import io.redstone.core.adaptor.base.OutboundAdaptor;
+import io.redstone.core.adaptor.api.Adaptor;
 
 public class AdaptorKeeper {
 
 	// 存储OutboundAdaptor, 使用subAccountId索引
-	private MutableIntObjectMap<OutboundAdaptor> outboundAdaptorMap = MutableMaps.newIntObjectHashMap();
+	private MutableIntObjectMap<Adaptor> outboundAdaptorMap = MutableMaps.newIntObjectHashMap();
 
 	private final static AdaptorKeeper InnerInstance = new AdaptorKeeper();
 
 	private AdaptorKeeper() {
 	}
 
-	public static OutboundAdaptor getOutboundAdaptor(int subAccountId) {
+	public static Adaptor getAdaptor(int subAccountId) {
 		return InnerInstance.outboundAdaptorMap.get(subAccountId);
 	}
 
-	public static OutboundAdaptor putOutboundAdaptor(int subAccountId, OutboundAdaptor adaptor) {
+	public static Adaptor putAdaptor(int subAccountId, Adaptor adaptor) {
 		return InnerInstance.outboundAdaptorMap.put(subAccountId, adaptor);
 	}
 
