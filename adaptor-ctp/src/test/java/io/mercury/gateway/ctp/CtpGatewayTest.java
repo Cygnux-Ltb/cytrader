7ce62bd936dc3c6a6b331bd8f4a4358d77031319
yellow013 <wk9988@gmail.com>
2020-03-31 23:13:48 +0800

    update ctp adaptor

--- adaptor-ctp/src/test/java/io/mercury/gateway/ctp/CtpGatewayTest.java
+++ adaptor-ctp/src/test/java/io/mercury/gateway/ctp/CtpGatewayTest.java
@@ -17,62 +17,63 @@ import io.mercury.ctp.gateway.bean.rsp.RtnTrade;
 
 public class CtpGatewayTest {
 
-    private final Logger log = CommonLoggerFactory.getLogger(CtpGatewayTest.class);
+	private final Logger log = CommonLoggerFactory.getLogger(CtpGatewayTest.class);
 
-    // 标准CTP
+	// 标准CTP
 //	private String TradeAddress = "tcp://180.168.146.187:10100";
 //	private String MdAddress = "tcp://180.168.146.187:10110";
 
-    // 7*24 CTP连通测试
-    private String TradeAddress = "tcp://180.168.146.187:10130";
-    private String MdAddress = "tcp://180.168.146.187:10131";
+	// 7*24 CTP连通测试
+	private String TradeAddress = "tcp://180.168.146.187:10130";
+	private String MdAddress = "tcp://180.168.146.187:10131";
 
-    private String BrokerId = "9999";
-    private String InvestorId = "132796";
-    private String UserId = "132796";
-    private String AccountId = "132796";
-    private String Password = "tc311911";
+	private String BrokerId = "9999";
+	private String InvestorId = "132796";
+	private String UserId = "132796";
+	private String AccountId = "132796";
+	private String Password = "tc311911";
 
-    private String TradingDay = "20200302";
-    private String CurrencyId = "CNY";
+	private String TradingDay = "20200302";
+	private String CurrencyId = "CNY";
 
-    private String GatewayId = "simnow_test";
+	private String GatewayId = "simnow_test";
 
-    @Test
-    public void test() {
+	@Test
+	public void test() {
 
-	CtpConfigInfo simnowUserInfo = new CtpConfigInfo().setTraderAddress(TradeAddress).setMdAddress(MdAddress)
-		.setBrokerId(BrokerId).setInvestorId(InvestorId).setUserId(UserId).setAccountId(AccountId)
-		.setPassword(Password).setTradingDay(TradingDay).setCurrencyId(CurrencyId);
+		CtpConfigInfo simnowUserInfo = new CtpConfigInfo().setTraderAddress(TradeAddress).setMdAddress(MdAddress)
+				.setBrokerId(BrokerId).setInvestorId(InvestorId).setUserId(UserId).setAccountId(AccountId)
+				.setPassword(Password).setTradingDay(TradingDay).setCurrencyId(CurrencyId);
 
-	CtpGateway gateway = new CtpGateway(GatewayId, simnowUserInfo,
-		MpscArrayBlockingQueue.autoStartQueue("Simnow-Handle-Queue", 1024, msg -> {
-		    switch (msg.type()) {
-		    case DepthMarketData:
-			RspDepthMarketData depthMarketData = msg.getRspDepthMarketData();
-			log.info(
-				"Handle CThostFtdcDepthMarketDataField -> InstrumentID==[{}]  UpdateTime==[{}]  UpdateMillisec==[{}]  AskPrice1==[{}]  BidPrice1==[{}]",
-				depthMarketData.getInstrumentID(), depthMarketData.getUpdateTime(),
-				depthMarketData.getUpdateMillisec(), depthMarketData.getAskPrice1(),
-				depthMarketData.getBidPrice1());
-			break;
-		    case RtnOrder:
-			RtnOrder order = msg.getRtnOrder();
-			log.info("Handle RtnOrder -> OrderRef==[{}]", order.getOrderRef());
-			break;
-		    case RtnTrade:
-			RtnTrade trade = msg.getRtnTrade();
-			log.info("Handle RtnTrade -> OrderRef==[{}]", trade.getOrderRef());
-			break;
-		    default:
-			break;
-		    }
-		}));
-	gateway.initAndJoin();
-	Set<String> instruementIdSet = new HashSet<>();
-	instruementIdSet.add("rb2010");
-	gateway.subscribeMarketData(instruementIdSet);
-	ThreadUtil.join();
-    }
+		CtpGateway gateway = new CtpGateway(GatewayId, simnowUserInfo,
+				MpscArrayBlockingQueue.autoStartQueue("Simnow-Handle-Queue", 1024, msg -> {
+					switch (msg.type()) {
+					case DepthMarketData:
+						RspDepthMarketData depthMarketData = msg.getRspDepthMarketData();
+						log.info(
+								"Handle CThostFtdcDepthMarketDataField -> InstrumentID==[{}]  UpdateTime==[{}]  UpdateMillisec==[{}]  AskPrice1==[{}]  BidPrice1==[{}]",
+								depthMarketData.getInstrumentID(), depthMarketData.getUpdateTime(),
+								depthMarketData.getUpdateMillisec(), depthMarketData.getAskPrice1(),
+								depthMarketData.getBidPrice1());
+						break;
+					case RtnOrder:
+						RtnOrder order = msg.getRtnOrder();
+						log.info("Handle RtnOrder -> OrderRef==[{}]", order.getOrderRef());
+						break;
+					case RtnTrade:
+						RtnTrade trade = msg.getRtnTrade();
+						log.info("Handle RtnTrade -> OrderRef==[{}]", trade.getOrderRef());
+						break;
+					default:
+						break;
+					}
+				}));
+		gateway.initAndJoin();
+		Set<String> instruementIdSet = new HashSet<>();
+		instruementIdSet.add("rb2010");
+		gateway.subscribeMarketData(instruementIdSet);
+		ThreadUtil.join();
+		
+	}
 
 }
