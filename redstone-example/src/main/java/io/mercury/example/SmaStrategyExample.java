1a76384c745e689f0731c5072ae25ceae792dbcd
yellow013 <wk9988@gmail.com>
2020-04-06 22:29:21 +0800

    update example

--- redstone-example/src/main/java/io/mercury/example/SmaStrategyExample.java
+++ redstone-example/src/main/java/io/mercury/example/SmaStrategyExample.java
@@ -61,4 +61,10 @@ public class SmaStrategyExample extends IndicatorStrategy<BasicMarketData> imple
 
 	}
 
+	@Override
+	public void addAdaptor(Adaptor adaptor) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
