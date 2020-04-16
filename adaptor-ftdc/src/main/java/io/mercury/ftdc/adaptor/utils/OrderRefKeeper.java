d2ee10cf38b88f9fe77fc774c5ffee79cc6c2bed
yellow013 <wk9988@gmail.com>
2020-04-16 23:49:18 +0800

    update adaptor utils

--- adaptor-ftdc/src/main/java/io/mercury/ftdc/adaptor/utils/OrderRefKeeper.java
+++ adaptor-ftdc/src/main/java/io/mercury/ftdc/adaptor/utils/OrderRefKeeper.java
@@ -1,11 +1,11 @@
-package io.mercury.ctp.adaptor.utils;
+package io.mercury.ftdc.adaptor.utils;
 
 import org.eclipse.collections.api.map.primitive.MutableLongObjectMap;
 import org.eclipse.collections.api.map.primitive.MutableObjectLongMap;
 
 import io.mercury.common.collections.Capacity;
 import io.mercury.common.collections.MutableMaps;
-import io.mercury.ctp.adaptor.exception.OrderRefNotFoundException;
+import io.mercury.ftdc.adaptor.exception.OrderRefNotFoundException;
 
 /**
  * 
