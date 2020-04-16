718b4fe9ed38ee0ad1db99597abf3795d421ae1a
yellow013 <wk9988@gmail.com>
2020-04-16 23:49:48 +0800

    update adaptor

--- adaptor-ftdc/src/main/java/io/mercury/ftdc/adaptor/FtdcAdaptor.java
+++ adaptor-ftdc/src/main/java/io/mercury/ftdc/adaptor/FtdcAdaptor.java
@@ -1,4 +1,4 @@
-package io.mercury.ctp.adaptor;
+package io.mercury.ftdc.adaptor;
 
 import static io.mercury.financial.util.PriceUtil.priceToLong4;
 
@@ -17,7 +17,6 @@ import org.slf4j.Logger;
 
 import ctp.thostapi.CThostFtdcInputOrderActionField;
 import ctp.thostapi.CThostFtdcInputOrderField;
-import ctp.thostapi.CThostFtdcOrderActionField;
 import io.mercury.common.concurrent.queue.MpscArrayBlockingQueue;
 import io.mercury.common.datetime.Pattern.DatePattern;
 import io.mercury.common.datetime.Pattern.TimePattern;
@@ -26,22 +25,23 @@ import io.mercury.common.datetime.TimeZone;
 import io.mercury.common.functional.Converter;
 import io.mercury.common.log.CommonLoggerFactory;
 import io.mercury.common.param.ImmutableParamMap;
-import io.mercury.ctp.adaptor.converter.CancelOrderConverter;
-import io.mercury.ctp.adaptor.converter.NewOrderConverter;
-import io.mercury.ctp.adaptor.exception.OrderRefNotFoundException;
-import io.mercury.ctp.adaptor.utils.OrderRefGenerate;
-import io.mercury.ctp.adaptor.utils.OrderRefKeeper;
-import io.mercury.ctp.gateway.CtpGateway;
-import io.mercury.ctp.gateway.bean.CtpConfigInfo;
-import io.mercury.ctp.gateway.bean.rsp.RspConnectInfo;
-import io.mercury.ctp.gateway.bean.rsp.RspDepthMarketData;
-import io.mercury.ctp.gateway.bean.rsp.RspOrderAction;
-import io.mercury.ctp.gateway.bean.rsp.RspOrderInsert;
-import io.mercury.ctp.gateway.bean.rsp.RtnOrder;
-import io.mercury.ctp.gateway.bean.rsp.RtnTrade;
 import io.mercury.financial.instrument.Instrument;
 import io.mercury.financial.instrument.InstrumentKeeper;
 import io.mercury.financial.market.impl.BasicMarketData;
+import io.mercury.ftdc.adaptor.converter.CancelOrderConverter;
+import io.mercury.ftdc.adaptor.converter.NewOrderConverter;
+import io.mercury.ftdc.adaptor.exception.OrderRefNotFoundException;
+import io.mercury.ftdc.adaptor.utils.OrderRefGenerate;
+import io.mercury.ftdc.adaptor.utils.OrderRefKeeper;
+import io.mercury.ftdc.gateway.FtdcGateway;
+import io.mercury.ftdc.gateway.bean.FtdcConfigInfo;
+import io.mercury.ftdc.gateway.bean.FtdcDepthMarketData;
+import io.mercury.ftdc.gateway.bean.FtdcInputOrder;
+import io.mercury.ftdc.gateway.bean.FtdcInputOrderAction;
+import io.mercury.ftdc.gateway.bean.FtdcOrder;
+import io.mercury.ftdc.gateway.bean.FtdcOrderAction;
+import io.mercury.ftdc.gateway.bean.FtdcTrade;
+import io.mercury.ftdc.gateway.bean.RspConnectInfo;
 import io.redstone.core.account.Account;
 import io.redstone.core.adaptor.base.BaseAdaptor;
 import io.redstone.core.order.Order;
@@ -49,16 +49,15 @@ import io.redstone.core.order.specific.ChildOrder;
 import io.redstone.core.order.structure.OrdReport;
 import io.redstone.core.strategy.StrategyScheduler;
 
-public class CtpAdaptor extends BaseAdaptor {
+public class FtdcAdaptor extends BaseAdaptor {
 
-	private static final Logger log = CommonLoggerFactory.getLogger(CtpAdaptor.class);
+	private static final Logger log = CommonLoggerFactory.getLogger(FtdcAdaptor.class);
 
 	private final DateTimeFormatter updateTimeformatter = TimePattern.HH_MM_SS.newFormatter();
 
 	private final DateTimeFormatter actionDayformatter = DatePattern.YYYYMMDD.newFormatter();
 
-	private Function<RspDepthMarketData, BasicMarketData> marketDataConverter = (
-			RspDepthMarketData depthMarketData) -> {
+	private Function<FtdcDepthMarketData, BasicMarketData> marketDataConverter = depthMarketData -> {
 
 		LocalDate depthDate = LocalDate.parse(depthMarketData.getActionDay(), actionDayformatter);
 		LocalTime depthTime = LocalTime.parse(depthMarketData.getUpdateTime(), updateTimeformatter)
@@ -76,74 +75,73 @@ public class CtpAdaptor extends BaseAdaptor {
 						.setAskVolume1(depthMarketData.getAskVolume1());
 	};
 
-	private Converter<RtnOrder, OrdReport> rtnOrderConverter = (rtnOrder, ordReport) -> {
+	private Converter<FtdcOrder, OrdReport> rtnOrderConverter = (ftdcOrder, ordReport) -> {
 		return ordReport;
 	};
 
-	private Converter<RtnTrade, OrdReport> rtnTradeConverter = (rtnTrade, ordReport) -> {
+	private Converter<FtdcTrade, OrdReport> rtnTradeConverter = (ftdcTrade, ordReport) -> {
 		return ordReport;
 	};
 
 	private int appId;
-	private final CtpGateway gateway;
+	private final FtdcGateway gateway;
 
 	private volatile int frontId;
 	private volatile int sessionId;
 
 	private volatile boolean isIsAvailable;
 
-	public CtpAdaptor(int adaptorId, String adaptorName, int appId, @Nonnull StrategyScheduler scheduler,
-			@Nonnull ImmutableParamMap<CtpAdaptorParam> paramMap) {
+	public FtdcAdaptor(int adaptorId, String adaptorName, int appId, @Nonnull StrategyScheduler scheduler,
+			@Nonnull ImmutableParamMap<FtdcAdaptorParam> paramMap) {
 		super(adaptorId, adaptorName);
 		this.appId = appId;
 		// 写入Gateway用户信息
-		CtpConfigInfo configInfo = new CtpConfigInfo()
-				.setTraderAddr(paramMap.getString(CtpAdaptorParam.CTP_TraderAddr))
-				.setMdAddr(paramMap.getString(CtpAdaptorParam.CTP_MdAddr))
-				.setBrokerId(paramMap.getString(CtpAdaptorParam.CTP_BrokerId))
-				.setInvestorId(paramMap.getString(CtpAdaptorParam.CTP_InvestorId))
-				.setUserId(paramMap.getString(CtpAdaptorParam.CTP_UserId))
-				.setAccountId(paramMap.getString(CtpAdaptorParam.CTP_AccountId))
-				.setPassword(paramMap.getString(CtpAdaptorParam.CTP_Password));
+		FtdcConfigInfo configInfo = new FtdcConfigInfo()
+				.setTraderAddr(paramMap.getString(FtdcAdaptorParam.CTP_TraderAddr))
+				.setMdAddr(paramMap.getString(FtdcAdaptorParam.CTP_MdAddr))
+				.setBrokerId(paramMap.getString(FtdcAdaptorParam.CTP_BrokerId))
+				.setInvestorId(paramMap.getString(FtdcAdaptorParam.CTP_InvestorId))
+				.setUserId(paramMap.getString(FtdcAdaptorParam.CTP_UserId))
+				.setAccountId(paramMap.getString(FtdcAdaptorParam.CTP_AccountId))
+				.setPassword(paramMap.getString(FtdcAdaptorParam.CTP_Password));
 
 		// 初始化Gateway
-		this.gateway = new CtpGateway("Ftdc-Gateway", configInfo,
-				MpscArrayBlockingQueue.autoStartQueue("Gateway-Handle-Queue", 1024, rspMsg -> {
-					switch (rspMsg.getRspMsgType()) {
-					case RspConnectInfo:
-						RspConnectInfo rspConnectInfo = rspMsg.getRspConnectInfo();
+		this.gateway = new FtdcGateway("Ftdc-Gateway", configInfo,
+				MpscArrayBlockingQueue.autoStartQueue("Gateway-Handle-Queue", 1024, ftdcMsg -> {
+					switch (ftdcMsg.getFtdcRespType()) {
+					case RespConnectInfo:
+						RspConnectInfo rspConnectInfo = ftdcMsg.getRspConnectInfo();
 						this.frontId = rspConnectInfo.getFrontID();
 						this.sessionId = rspConnectInfo.getSessionID();
 						this.isIsAvailable = rspConnectInfo.isAvailable();
 						break;
-					case DepthMarketData:
-						BasicMarketData marketData = marketDataConverter.apply(rspMsg.getRspDepthMarketData());
+					case FtdcDepthMarketData:
+						BasicMarketData marketData = marketDataConverter.apply(ftdcMsg.getFtdcDepthMarketData());
 						scheduler.onMarketData(marketData);
 						break;
-					case RtnOrder:
-						RtnOrder ftdcRtnOrder = rspMsg.getRtnOrder();
-						OrdReport rtnOrder = checkoutCtpOrder(ftdcRtnOrder.getOrderRef());
-						scheduler.onOrderReport(rtnOrderConverter.conversion(ftdcRtnOrder, rtnOrder));
+					case FtdcOrder:
+						FtdcOrder ftdcOrder = ftdcMsg.getFtdcOrder();
+						OrdReport rtnOrder = checkoutCtpOrder(ftdcOrder.getOrderRef());
+						scheduler.onOrderReport(rtnOrderConverter.conversion(ftdcOrder, rtnOrder));
 						break;
-					case RtnTrade:
-						RtnTrade ftdcRtnTrade = rspMsg.getRtnTrade();
-						OrdReport rtnTrade = checkoutCtpOrder(ftdcRtnTrade.getOrderRef());
-						scheduler.onOrderReport(rtnTradeConverter.conversion(ftdcRtnTrade, rtnTrade));
+					case FtdcTrade:
+						FtdcTrade ftdcTrade = ftdcMsg.getFtdcTrade();
+						OrdReport rtnTrade = checkoutCtpOrder(ftdcTrade.getOrderRef());
+						scheduler.onOrderReport(rtnTradeConverter.conversion(ftdcTrade, rtnTrade));
 						break;
-					case RspOrderInsert:
-						RspOrderInsert ftdcRspOrderInsert = rspMsg.getRspOrderInsert();
-						
-						break;
-					case RspOrderAction:
-						RspOrderAction ftdcRspOrderAction = rspMsg.getRspOrderAction();
-						break;
-					case ErrRtnOrderInsert:
-						CThostFtdcInputOrderField errRtnOrderInsert = rspMsg.getErrRtnOrderInsert();
+					// 报单错误处理
+					case FtdcInputOrder:
+					case ErrFtdcInputOrder:
+						FtdcInputOrder ftdcInputOrder = ftdcMsg.getFtdcInputOrder();
 
 						break;
-					case ErrRtnOrderAction:
-						CThostFtdcOrderActionField errRtnOrderAction = rspMsg.getErrRtnOrderAction();
-
+					// 撤单错误处理1
+					case FtdcInputOrderAction:
+						FtdcInputOrderAction ftdcInputOrderAction = ftdcMsg.getFtdcInputOrderAction();
+						break;
+					// 撤单错误处理1
+					case ErrFtdcOrderAction:
+						FtdcOrderAction errRtnOrderInsert = ftdcMsg.getFtdcOrderAction();
 						break;
 					default:
 						break;
--- adaptor-ftdc/src/main/java/io/mercury/ftdc/adaptor/FtdcAdaptorParam.java
+++ adaptor-ftdc/src/main/java/io/mercury/ftdc/adaptor/FtdcAdaptorParam.java
@@ -1,26 +1,34 @@
-package io.mercury.ctp.adaptor;
+package io.mercury.ftdc.adaptor;
 
 import io.mercury.common.param.api.ParamKey;
 import io.mercury.common.param.api.ParamType;
 
-public enum CtpAdaptorParam implements ParamKey {
+public enum FtdcAdaptorParam implements ParamKey {
 
 	/**
 	 * CTP Params
 	 */
-	CTP_TraderAddr(201, "ctp.trader.addr", ParamType.STRING),
+	CTP_TraderAddr(201, "trader.addr", ParamType.STRING),
 
-	CTP_MdAddr(202, "ctp.md.addr", ParamType.STRING),
+	CTP_MdAddr(202, "md.addr", ParamType.STRING),
 
-	CTP_BrokerId(203, "ctp.broker.id", ParamType.STRING),
+	CTP_AppId(203, "app.id", ParamType.STRING),
 
-	CTP_InvestorId(204, "ctp.investor.id", ParamType.STRING),
+	CTP_BrokerId(204, "broker.id", ParamType.STRING),
 
-	CTP_UserId(205, "ctp.user.id", ParamType.STRING),
+	CTP_InvestorId(205, "investor.id", ParamType.STRING),
 
-	CTP_AccountId(206, "ctp.account.id", ParamType.STRING),
+	CTP_AccountId(206, "account.id", ParamType.STRING),
 
-	CTP_Password(207, "ctp.password", ParamType.STRING),
+	CTP_UserId(207, "user.id", ParamType.STRING),
+
+	CTP_Password(208, "password", ParamType.STRING),
+
+	CTP_AuthCode(209, "auth.code", ParamType.STRING),
+
+	CTP_IpAddr(210, "ip.addr", ParamType.STRING),
+
+	CTP_MacAddr(211, "mac.addr", ParamType.STRING),
 
 	;
 
@@ -28,7 +36,7 @@ public enum CtpAdaptorParam implements ParamKey {
 	private String paramName;
 	private ParamType paramType;
 
-	private CtpAdaptorParam(int paramId, String paramName, ParamType paramType) {
+	private FtdcAdaptorParam(int paramId, String paramName, ParamType paramType) {
 		this.paramId = paramId;
 		this.paramName = paramName;
 		this.paramType = paramType;
