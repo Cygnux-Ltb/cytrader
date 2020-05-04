918438e8a62e8b29624f6792baf338ff2ab0e0fb
yellow013 <wk9988@gmail.com>
2020-05-04 22:17:17 +0800

    update ib adaptor

---
+++ adaptor-ib/src/main/java/io/mercury/ib/launch/IbAdaptorStartup.java
@@ -0,0 +1,9 @@
+package io.mercury.ib.launch;
+
+public class IbAdaptorStartup {
+
+	public static void main(String[] args) {
+
+	}
+
+}
--- adaptor-ib/src/test/java/io/mercury/ib/adaptor/AppTest.java
+++ adaptor-ib/src/test/java/io/mercury/ib/adaptor/AppTest.java
@@ -1,4 +1,4 @@
-package io.redstone.adaptor.ib;
+package io.mercury.ib.adaptor;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -7,32 +7,27 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 }
