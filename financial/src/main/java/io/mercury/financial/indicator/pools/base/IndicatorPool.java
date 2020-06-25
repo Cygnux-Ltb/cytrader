4b18415fafae25ce454b6f158a7b6eb69702d7e2
yellow013 <wk9988@gmail.com>
2020-06-25 19:29:53 +0800

    update indicator

--- financial/src/main/java/io/mercury/financial/indicator/pools/base/IndicatorPool.java
+++ financial/src/main/java/io/mercury/financial/indicator/pools/base/IndicatorPool.java
@@ -10,7 +10,7 @@ import io.mercury.financial.market.api.MarketData;
 
 public abstract class IndicatorPool<I extends Indicator<?, ?, M>, M extends MarketData> {
 
-	protected Logger log = CommonLoggerFactory.getLogger(getClass());
+	protected final Logger log = CommonLoggerFactory.getLogger(getClass());
 
 	protected MutableList<I> indicators = MutableLists.newFastList();
 
