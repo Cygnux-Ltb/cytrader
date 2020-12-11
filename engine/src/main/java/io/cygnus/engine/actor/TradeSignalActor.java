19d4dae8f77ab8fe9a7e8dedd68a435d91e85fee
yellow013 <wk9988@gmail.com>
2020-12-11 23:59:46 +0800

    update actor

--- engine/src/main/java/io/cygnus/engine/actor/TradeSignalActor.java
+++ engine/src/main/java/io/cygnus/engine/actor/TradeSignalActor.java
@@ -2,20 +2,20 @@ package io.cygnus.engine.actor;
 
 import org.eclipse.collections.api.list.MutableList;
 
-import io.horizon.definition.order.TradeSignal;
+import io.horizon.definition.order.TrdSignal;
 import io.mercury.common.collections.MutableLists;
 
 public final class TradeSignalActor {
 
-	private MutableList<TradeSignal> tradeSignalList = MutableLists.newFastList(256);
+	private MutableList<TrdSignal> tradeSignalList = MutableLists.newFastList(256);
 
 	public static final TradeSignalActor Singleton = new TradeSignalActor();
 
-	public boolean addTradeSignal(TradeSignal signal) {
+	public boolean addTradeSignal(TrdSignal signal) {
 		return tradeSignalList.add(signal);
 	}
 
-	public void handleTradeSignal(TradeSignal signal) {
+	public void handleTradeSignal(TrdSignal signal) {
 		switch (signal.action()) {
 		case Open:
 			switch (signal.direction()) {
