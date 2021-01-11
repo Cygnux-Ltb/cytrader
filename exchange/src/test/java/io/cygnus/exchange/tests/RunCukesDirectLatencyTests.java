19eb7d03b7c91fb26eb8feff7c3e49c72af2d566
yellow013 <wk9988@gmail.com>
2021-01-11 17:55:41 +0800

    update test

--- exchange/src/test/java/io/cygnus/exchange/tests/RunCukesDirectLatencyTests.java
+++ exchange/src/test/java/io/cygnus/exchange/tests/RunCukesDirectLatencyTests.java
@@ -1,13 +1,13 @@
-package exchange.core2.tests;
+package io.cygnus.exchange.tests;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
-import exchange.core2.core.common.config.PerformanceConfiguration;
-import exchange.core2.tests.steps.OrderStepdefs;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import io.cygnus.exchange.core.common.config.PerformanceConfiguration;
+import io.cygnus.exchange.tests.steps.OrderStepdefs;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = {"pretty", "html:target/cucumber"}, strict = true)
