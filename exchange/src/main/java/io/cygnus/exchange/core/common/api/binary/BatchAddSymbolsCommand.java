450ee214f22e637b4798eb3680b09e97fcfc0665
yellow013 <wk9988@gmail.com>
2021-01-16 16:44:41 +0800

    update api binary

--- exchange/src/main/java/io/cygnus/exchange/core/common/api/binary/BatchAddSymbolsCommand.java
+++ exchange/src/main/java/io/cygnus/exchange/core/common/api/binary/BatchAddSymbolsCommand.java
@@ -1,18 +1,3 @@
-/**
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package io.cygnus.exchange.core.common.api.binary;
 
 import io.cygnus.exchange.core.common.CoreSymbolSpecification;
@@ -28,11 +13,11 @@ import org.eclipse.collections.impl.map.mutable.primitive.IntObjectHashMap;
 
 import java.util.Collection;
 
-@AllArgsConstructor
 @EqualsAndHashCode
-@Getter
+@AllArgsConstructor
 public final class BatchAddSymbolsCommand implements BinaryDataCommand {
 
+	@Getter
 	private final MutableIntObjectMap<CoreSymbolSpecification> symbols;
 
 	public BatchAddSymbolsCommand(final CoreSymbolSpecification symbol) {
@@ -41,7 +26,7 @@ public final class BatchAddSymbolsCommand implements BinaryDataCommand {
 
 	public BatchAddSymbolsCommand(final Collection<CoreSymbolSpecification> collection) {
 		symbols = new IntObjectHashMap<>(collection.size());
-		collection.forEach(s -> symbols.put(s.symbolId, s));
+		collection.forEach(spec -> symbols.put(spec.symbolId, spec));
 	}
 
 	public BatchAddSymbolsCommand(final BytesIn<?> bytes) {
--- exchange/src/main/java/io/cygnus/exchange/core/common/api/binary/BinaryCommandType.java
+++ exchange/src/main/java/io/cygnus/exchange/core/common/api/binary/BinaryCommandType.java
@@ -1,23 +1,8 @@
-/**
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- */
 package io.cygnus.exchange.core.common.api.binary;
 
 import lombok.Getter;
 
-@Getter
+
 public enum BinaryCommandType {
 
     ADD_ACCOUNTS(1002),
@@ -26,14 +11,14 @@ public enum BinaryCommandType {
     
     ;
 
+	@Getter
     private final int code;
 
-    BinaryCommandType(int code) {
+    private BinaryCommandType(int code) {
         this.code = code;
     }
 
     public static BinaryCommandType of(int code) {
-
         switch (code) {
             case 1002:
                 return ADD_ACCOUNTS;
@@ -42,7 +27,6 @@ public enum BinaryCommandType {
             default:
                 throw new IllegalArgumentException("unknown BinaryCommandType:" + code);
         }
-
     }
 
 }
