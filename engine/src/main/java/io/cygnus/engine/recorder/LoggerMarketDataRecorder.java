556f80221f7ebcc856b40050ab1e2dfeb53cdf5b
yellow013 <wk9988@gmail.com>
2021-05-16 23:28:27 +0800

    update recorder

---
+++ engine/src/main/java/io/cygnus/engine/recorder/LoggerMarketDataRecorder.java
@@ -0,0 +1,30 @@
+package io.cygnus.engine.recorder;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+
+import io.horizon.market.data.impl.BasicMarketData;
+import io.horizon.market.instrument.Instrument;
+import io.horizon.transaction.event.MarketDataRecorder.AbstractMarketDataRecorder;
+import io.mercury.common.log.CommonLoggerFactory;
+
+public final class LoggerMarketDataRecorder extends AbstractMarketDataRecorder<BasicMarketData> {
+
+	private static final Logger log = CommonLoggerFactory.getLogger(LoggerMarketDataRecorder.class);
+
+	public LoggerMarketDataRecorder(Instrument... instruments) {
+		super(instruments);
+	}
+
+	@Override
+	public void onMarketData(BasicMarketData marketData) {
+		log.info("MarketData Recorde -> {}", marketData);
+	}
+
+	@Override
+	public void close() throws IOException {
+		log.info("LoggerMarketDataRecorder is closed");
+	}
+
+}
