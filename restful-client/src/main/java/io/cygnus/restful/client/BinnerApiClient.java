4efb9f6ac7a13c391353706646a3b41cd2b0e34b
yellow013 <wk9988@gmail.com>
2021-07-12 01:44:16 +0800

    update client

--- restful-client/src/main/java/io/cygnus/restful/client/BinnerApiClient.java
+++ restful-client/src/main/java/io/cygnus/restful/client/BinnerApiClient.java
@@ -2,21 +2,22 @@ package io.cygnus.restful.client;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import io.cygnus.repository.entity.Bar;
 import io.cygnus.restful.client.base.BaseApiClient;
 import io.cygnus.restful.client.base.PathParam;
 
+@Component
 public class BinnerApiClient extends BaseApiClient {
 
-	private String baseUri = "/binner";
+	private String baseUri = "/bar";
 
 	private String getTimeBinnersUri = baseUri + "/{cygId}?tradingDay={tradingDay}&instrumentId={instrumentId}";
 
 	public List<Bar> getTimeBinners(Integer cygId, String instrumentId, String tradingDay) {
-		return getResultSet(Bar.class, getTimeBinnersUri, 
-				new PathParam("cygId", cygId.toString()),
-				new PathParam("tradingDay", tradingDay), 
-				new PathParam("instrumentId", instrumentId));
+		return getResultSet(Bar.class, getTimeBinnersUri, new PathParam("cygId", cygId.toString()),
+				new PathParam("tradingDay", tradingDay), new PathParam("instrumentId", instrumentId));
 	}
 
 }
