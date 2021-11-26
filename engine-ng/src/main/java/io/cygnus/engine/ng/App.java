5fcc15a764023ee18a0bd05462b39ee445eda035
yellow013 <wk9988@gmail.com>
2021-11-26 23:59:21 +0800

    update app

--- engine-ng/src/main/java/io/cygnus/engine/ng/App.java
+++ engine-ng/src/main/java/io/cygnus/engine/ng/App.java
@@ -4,10 +4,10 @@ package io.cygnus.engine.ng;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
 }
