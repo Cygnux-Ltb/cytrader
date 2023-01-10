ff14e82addf00ffcdb86411b44cae978ad032ff9
Yellow013 <wk9988@gmail.com>
2023-01-10 09:36:38 +0900

    update

--- repository/src/main/java/io/cygnux/repository/constant/CommonQueryColumn.java
+++ repository/src/main/java/io/cygnux/repository/constant/CommonQueryColumn.java
@@ -1,23 +1,45 @@
 package io.cygnux.repository.constant;
 
-public interface RdbColumn {
+public interface QueryColumn {
 
-    String UID = "uid";
-
+    /**
+     * 策略ID
+     */
     String STRATEGY_ID = "strategy_id";
 
+    /**
+     * 策略名称
+     */
     String STRATEGY_NAME = "strategy_name";
 
-    String BROKER_ID = "broker_id";
+    /**
+     * 经纪商
+     */
+    String BROKER = "broker";
 
+    /**
+     * 投资者ID
+     */
     String INVESTOR_ID = "investor_id";
 
+    /**
+     * 账户ID
+     */
     String ACCOUNT_ID = "account_id";
 
+    /**
+     * 子账户ID
+     */
     String SUB_ACCOUNT_ID = "sub_account_id";
 
+    /**
+     * 交易标的代码
+     */
     String INSTRUMENT_CODE = "instrument_code";
 
+    /**
+     * 交易日
+     */
     String TRADING_DAY = "trading_day";
 
 }
